78166b292518d6ee3a94c62c2bdf2987f0ce7f96
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-01-26 22:28:27 +0100

    Slightly extended setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gpx_exporter',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GPL v3',
+    author='Patrick Westphal',
+    author_email='patrick.westphal@informatik.uni-leipzig.de',
+    description='',
+    install_requires=[
+    ],
+)
